985cd3d90a6675f562670a2822bea623fb0c8d83
Nelson Bassey <nellybaz10@gmail.com>
2021-06-08 22:54:30 +0200

    Adds more repository methods

--- src/repository/crud.repository.ts
+++ src/repository/crud.repository.ts
@@ -3,25 +3,32 @@ const Schema = mongoose.Schema;
 import { IDataSource, IRepository, RepositoryParameter } from '../interface';
 import { MongoDBDataSource } from '../datasources/mongodb.datasource';
 
-
 export class CrudRepository implements IRepository {
     db: IDataSource;
     model: Model<any, any, any>;
 
-    constructor({schema, modelName}:RepositoryParameter) {
+    constructor({ schema, modelName }: RepositoryParameter) {
         this.db = new MongoDBDataSource();
         this.model = mongoose.model(modelName, schema);
     }
 
-    async findOne(data: any) {
+    async findOne(data: mongoose.FilterQuery<any>) {
         return this.model.findOne(data);
     }
 
-    async updateOne(data: any) {
+    async findById(id: string) {
+        return this.model.findById(id);
+    }
+
+    async updateOne(data: mongoose.FilterQuery<any>) {
         return this.model.updateOne(data);
     }
 
-    async modelObject(){
-      return this.model;
+    async updateMany(data: mongoose.FilterQuery<any>) {
+        return this.model.updateMany(data);
+    }
+
+    async modelObject() {
+        return this.model;
     }
 }
