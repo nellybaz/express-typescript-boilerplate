7b4268d12c094c42f5f8a63a48ac49272c2b1642
Nelson Bassey <nellybaz10@gmail.com>
2021-06-08 22:27:47 +0200

    Adds datasource and repository interface

--- config/index.ts
+++ config/index.ts
@@ -4,10 +4,13 @@ dotenv.config();
 
 const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME || 'localhost';
 const SERVER_PORT = process.env.SERVER_PORT || 8000;
+const DB_URL = process.env.DB_URL || ''
+
 
 const SERVER = {
     hostname: SERVER_HOSTNAME,
-    port: SERVER_PORT
+    port: SERVER_PORT,
+    dbUrl: DB_URL
 };
 
 const config = {
