144d6100b3b99c5b3e3019719ae1a84128258220
Nelson Bassey <nellybaz10@gmail.com>
2021-06-08 23:11:25 +0200

    Refactored sample repo [git add .]

---
+++ src/repository/sample.repository.ts
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+import { CrudRepository } from './crud.repository';
+
+const SampleSchema = new Schema({
+    email: String,
+    passwordHash: String,
+    dateCreated: Date,
+    dateUpdate: Date
+});
+
+export class SampleRepository extends CrudRepository {
+
+    constructor() {
+      super({schema: SampleSchema, modelName:'User'});
+    }
+
+}
