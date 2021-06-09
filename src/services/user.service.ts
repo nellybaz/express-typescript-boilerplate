1f6ee6ab5ca35f305c2f7a52d2343b3742a5514a
Fawas kareem <phawazzzy@gmail.com>
2021-06-09 21:05:19 +0100

    created method for user register

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,7 +1,7 @@
 import AuthHelper from "../helpers/authHelpers";
 import { UserRepositry } from "../repository/user.repository";
-import { Unauthorized } from "http-errors";
 import { IResponse } from "../interfaces/response.interface";
+import { Conflict, InternalServerError, Unauthorized } from "http-errors";
 
 interface Login {
     email: string,
@@ -40,7 +40,53 @@ export class UserServivce {
                 message: "Authentication successfull",
                 error: null
             };
-        } catch (error) {
+        } catch (error:any) {
+            return {
+                status: false,
+                statusCode: error.status || 400,
+                data: null,
+                message: error.message,
+                error
+            };
+        }
+    }
+
+    // handles user register
+    async userRegister(payload: Login) {
+        try {
+            const { email, password } = payload;
+
+            // check if that email exist
+            const existingUser = await this._repo.findOne({ email });
+            if (existingUser) {
+                throw new Conflict("Email exist");
+            }
+
+            // hash password
+            const passwordHash = await AuthHelper.hashPassword(password);
+
+            // create the user
+            const user = await this._repo.create({ email, passwordHash });
+            if (!user) {
+                throw new InternalServerError("Unable to save user's data");
+            }
+
+            // create token
+            const token = await AuthHelper.generateToken({ userId: user._id });
+
+            // return the response
+            return {
+                status: true,
+                statusCode: 201,
+                data: {
+                    email: user.email,
+                    token
+                },
+                message: "User registration successfull",
+                error: null
+            }
+
+        } catch (error:any) {
             return {
                 status: false,
                 statusCode: error.status || 400,
