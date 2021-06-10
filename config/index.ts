776fac148cf188947956d4f5590b92d70eb3ff65
Nelson Bassey <nellybaz10@gmail.com>
2021-06-10 23:02:25 +0200

    Add sendgrid email sending service

--- config/index.ts
+++ config/index.ts
@@ -6,13 +6,15 @@ const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME || 'localhost';
 const SERVER_PORT = process.env.SERVER_PORT || 8000;
 const DB_URL = process.env.DB_URL || 'mongodb://localhost:27017/mydb';
 const JWT_SECRET = process.env.JWT_SECRET || 'secretkeyHereForDev'
+const SENDGRID_KEY = process.env.SENDGRID_API_KEY || '';
 
 
 const SERVER = {
     hostname: SERVER_HOSTNAME,
     port: SERVER_PORT,
     dbUrl: DB_URL,
-    jwtSecret: JWT_SECRET
+    jwtSecret: JWT_SECRET,
+    sendGridKey: SENDGRID_KEY
 };
 
 const config = {
--- src/services/inbuilt-email.service.ts
+++ src/services/inbuilt-email.service.ts
@@ -1,7 +1,24 @@
-import { IEmailService, ISendEmailBody } from "../interfaces/emailservice.interface";
+import { injectable } from 'inversify';
+import { IEmailService, ISendEmailBody } from '../interfaces/emailservice.interface';
+import mail from '@sendgrid/mail';
+import config from '../../config';
 
-export class InbuiltEmailService implements IEmailService{
-  async sendEmail(data: ISendEmailBody){
-    return true
-  }
+@injectable()
+export class InbuiltEmailService implements IEmailService {
+    async sendEmail(data: ISendEmailBody) {
+        try {
+            mail.setApiKey(config.server.sendGridKey);
+            const msg = {
+                to: data.receiver,
+                from: 'contract@talentmatch.africa', // Use the email address or domain you verified above
+                subject: data.subject,
+                text: data.body
+                // html: '<strong>and easy to do anywhere, even with Node.js</strong>'
+            };
+            await mail.send(msg);
+        } catch (error) {
+            return false;
+        }
+        return true;
+    }
 }
